1cfd6c2f7de533e7142a4513e8ccbde10124250d
KazukiDaido <k10de302222@gmail.com>
2023-02-15 04:04:00 +0900

    カテゴリーフィルター追加

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -13,6 +13,7 @@ struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
     @State private var searchText = ""
+    @State private var showCategoriesOnly = ""
     
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
@@ -23,30 +24,56 @@ struct LandmarkList: View {
     var searchResults: [Landmark] {
         if searchText.isEmpty {
             return filteredLandmarks
-        } else {
+        }else {
             return filteredLandmarks.filter { landmark in
                 return landmark.name.contains(searchText) }
         }
     }
     
+    var filteredCategories: [Landmark] {
+        if showCategoriesOnly != "" {
+            return searchResults.filter { landmark in
+                return landmark.category.rawValue.contains(showCategoriesOnly) }
+        } else {
+            return searchResults
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                Toggle(isOn: $showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-                ForEach(searchResults) { landmark in
-                    NavigationLink {
-                        LandmarkDetail(landmark: landmark)
-                    } label: {
-                        LandmarkRow(landmark: landmark)
+            VStack{
+                HStack{
+                    Button("Lakes"){
+                        showCategoriesOnly = "Lakes"
+                    }
+                    Button("Mountains"){
+                        showCategoriesOnly = "Mountains"
+                    }
+                    Button("Rivers"){
+                        showCategoriesOnly = "Rivers"
+                    }
+                    Button("Cancel"){
+                        showCategoriesOnly = ""
                     }
                 }
+                List {
+                    Toggle(isOn: $showFavoritesOnly) {
+                        Text("Favorites only")
+                    }
+                    
+                    ForEach(filteredCategories) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
-        }
-        .searchable(text: $searchText)
     }
+        .searchable(text: $searchText)
+}
 }
 
 struct LandmarkList_Previews: PreviewProvider {
